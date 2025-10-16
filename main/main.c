910b9f70959d93de420cc593c43a24725f8e54fa
Juli Gassibe <gassibe.julian@uncuyo.edu.ar>
2025-10-15 23:51:17 -0300

    boka lcdtm

--- main/main.c
+++ main/main.c
@@ -6,6 +6,7 @@
 #include "initTimers.h"
 #include "initSPI.h"
 #include "initI2C.h"
+#include "esp_timer.h"
 
 spi_device_handle_t mt6835;
 i2c_master_bus_handle_t busI2C;
@@ -14,26 +15,24 @@ i2c_master_dev_handle_t ina3221;
 void app_main(void) {
     ESP_ERROR_CHECK(initGPIO());
     //ESP_ERROR_CHECK(initTimers());
-    ESP_ERROR_CHECK(initSPI());
+    //ESP_ERROR_CHECK(initSPI());
     ESP_ERROR_CHECK(initI2C());
 
-    uint16_t config = CH1_EN | CH2_EN | CH3_EN | AVG_4_SAMPLE | BUS_CT_1MS | SHUNT_CT_1MS | MODE_SHUNT_CONT | MODE_BUS_CONT;
-    
+    uint16_t config = CH1_EN | CH2_EN | CH3_EN | AVG_1_SAMPLE | SHUNT_CT_140US | MODE_SHUNT_CONT;
+
+    printf("Config %d\n", config);
+
     ina3221_set_config(&ina3221, config);
 
-    int16_t shunt1 = 0, bus1 = 0, shunt2 = 0, bus2 = 0, shunt3 = 0, bus3 = 0;
+    int16_t shunt1 = 0, shunt2 = 0, shunt3 = 0;
+
+    uint64_t inicio = esp_timer_get_time();
 
     ina3221_get_ch1_shunt(&ina3221, &shunt1);
-    ina3221_get_ch1_bus(&ina3221, &bus1);
     ina3221_get_ch2_shunt(&ina3221, &shunt2);
-    ina3221_get_ch2_bus(&ina3221, &bus2);
     ina3221_get_ch3_shunt(&ina3221, &shunt3);
-    ina3221_get_ch3_bus(&ina3221, &bus3);
 
-    printf("Corriente 1: %f\n", shunt1*0.000040 / 0.01);
-    printf("Bus 1: %f\n", bus1*0.008);
-    printf("Corriente 2: %f\n", shunt2*0.000040 / 0.01);
-    printf("Bus 2: %f\n", bus2*0.008);
-    printf("Corriente 3: %f\n", shunt3*0.000040 / 0.01);
-    printf("Bus 3: %f\n", bus3*0.008);
+    uint64_t fin = esp_timer_get_time();
+
+    printf("Demora: %lld\n", fin-inicio);
 }
